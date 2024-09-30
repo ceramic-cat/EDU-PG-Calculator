f414af38e92ba5aa3c3f56d9a6c3c7d043f9eeb8
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2024-09-30 08:39:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+            // Lets make a calculator shall we
+
+            // Uppgift bas: Gör mkt enkelt program och gör div git-grejer med det
+
+
+            // Lets do a calculatoooor
+
+            // Nästa uppgift: "Grafiskt interface"
+
+
+
+            // TODO
+            // Klass som gör matematik
+
+            // Om jag orkar, gör med valbar RPN/normal style ;)
+
+            // Möjliga operationer:
+            // +, -, *, /, %, 
+
+            // Metod för inmatning av siffror (floats?) med kontroll av att det är faktiska siffror
+            // Metod för inmatning av operationer, valbara med char? Eller ur meny?
+
+
+
+
+
+
+
+        }
+        class Calculator
+        {
+            //private float _firstValue;
+            //private float _secondValue;
+            //private char _operator;
+
+            public float FirstValue { get; private set; }
+            public float SecondValue { get; private set; }
+            public float Operator { get; private set; }
+
+
+            public Calculator()
+            {
+
+            }
+
+            // Lista eller dictionary med olika operatorer?
+
+
+
+        }
+
+
+    }
+
+
+
+
+}
